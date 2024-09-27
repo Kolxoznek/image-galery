1d316fbff0e51660bfe7cfb3e63db391c26b6587
Evgeniy <defender35185@gmail.com>
2024-09-27 20:23:09 +0300

    feat: complete image galery task

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 const input = document.querySelector('.search__input');
-const imageContainers = document.querySelectorAll('.image-container');
-let str = ''
-input.addEventListener('input', () => {
-    str = input.value;
+const imageContainer = document.querySelector('.grid');
+const resetBtn = document.querySelector('.search__reset');
+const emptyPlaceholder = document.createElement('h2');
+emptyPlaceholder.classList.add('empty-placeholder');
+emptyPlaceholder.textContent = 'По вашему запросу не найдено изображений.'
+let str = 'winter'
+
+async function getData() {
+    const res = await fetch(`https://api.unsplash.com/search/photos?query=${str}&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`)
+    const data = await res.json();
+
+    if (data.results.length === 0) {
+        imageContainer.append(emptyPlaceholder);
+    } else {
+        data.results.forEach((image) => {
+            renderImage(`${image.urls.regular}`)
+        })
+    }
+}
+
+function renderImage(src) {
+    const elem = document.createElement('div');
+    elem.classList.add('image')
+    elem.style.backgroundImage = `url('${src}')`;
+    imageContainer.append(elem);
+}
+
+document.forms[0].addEventListener('submit', (e) => {
+    e.preventDefault();
+    str = document.forms[0].str.value;
+    imageContainer.innerHTML = ''
     getData();
 })
 
-async function getData() {
-    const res = await fetch(`https://api.unsplash.com/search/photos?query=${str}&per_page=9&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`)
-    const data = await res.json();
-    console.log(data)
-    fillImages(data)
-}
+input.addEventListener('input', () => {
+    if (input.value === '') {
+        resetBtn.classList.add('hide')
+    } else {
+        resetBtn.classList.remove('hide')
+    }
+})
 
-function fillImages(data) {
-    imageContainers.forEach((container, i) => {
-        container.style.background = `url('${data.results[i].urls.regular}')`
-    })
-}
+resetBtn.addEventListener('click', () =>  resetBtn.classList.add('hide'));
+
+getData();
